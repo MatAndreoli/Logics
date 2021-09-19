c5a89366f479fd9beac40b67d78a2ee87b34ec5b
Matheus Andreoli Vieira <andreoli.2003@gmail.com>
2021-09-19 15:22:44 -0300

    Update Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -18,7 +18,7 @@ namespace APP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            Application.Run(new FrmLogin());
         }
     }
 }
